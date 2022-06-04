ec54a1848817422318c5b0b002888aff048b23b8
Aaron Sutula <asutula@users.noreply.github.com>
2022-06-04 18:16:59 -0500

    test fix
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>

--- internal/staging/tableland/store/sqlite/sqlite_store_test.go
+++ internal/staging/tableland/store/sqlite/sqlite_store_test.go
@@ -8,10 +8,10 @@ import (
 )
 
 func TestGetOriginalRigs(t *testing.T) {
-	s, err := NewSQLiteStore("/Users/aaron/Code/textile/nft-minter/inventory.db", false)
+	s, err := NewSQLiteStore("../../../../../inventory.db", false)
 	require.NoError(t, err)
 
 	originals, err := s.GetOriginalRigs(context.Background())
 	require.NoError(t, err)
-	require.Len(t, originals, 185)
+	require.Len(t, originals, 181)
 }
