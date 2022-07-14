019b828fe9979dd03103468a00d5b3315cee384b
Andrew Hill <andrew@textile.io>
2022-07-14 14:00:33 -0700

    fix(metadata): cleanup rig name for opensea
    Signed-off-by: Andrew Hill <andrew@textile.io>

--- test/TablelandRigs.ts
+++ test/TablelandRigs.ts
@@ -77,7 +77,7 @@ describe("Rigs", function () {
       tablelandHost +
       "/query?mode=list&s=" +
       encodeURIComponent(
-        `select json_object('name','#'||id,'external_url','https://tableland.xyz/rigs/'||id,'image',image,'image_alpha',image_alpha,'thumb',thumb,'thumb_alpha',thumb_alpha,'attributes',json_group_array(json_object('display_type','text','trait_type','status','value','pre-reveal'))) from table1 where id=`
+        `select json_object('name','Rig #'||id,'external_url','https://tableland.xyz/rigs/'||id,'image',image,'image_alpha',image_alpha,'thumb',thumb,'thumb_alpha',thumb_alpha,'attributes',json_group_array(json_object('display_type','text','trait_type','status','value','pre-reveal'))) from table1 where id=`
       );
     await expect(uri[0]).to.equal(result);
     await expect(uri[1]).to.equal("%3B");
@@ -92,7 +92,7 @@ describe("Rigs", function () {
       tablelandHost +
       "/query?mode=list&s=" +
       encodeURIComponent(
-        `select json_object('name','#'||id,'external_url','https://tableland.xyz/rigs/'||id,'image',image,'image_alpha',image_alpha,'thumb',thumb,'thumb_alpha',thumb_alpha,'attributes',json_group_array(json_object('display_type',display_type,'trait_type',trait_type,'value',value))) from table1 join table2 on table1.id=table2.rig_id where id=`
+        `select json_object('name','Rig #'||id,'external_url','https://tableland.xyz/rigs/'||id,'image',image,'image_alpha',image_alpha,'thumb',thumb,'thumb_alpha',thumb_alpha,'attributes',json_group_array(json_object('display_type',display_type,'trait_type',trait_type,'value',value))) from table1 join table2 on table1.id=table2.rig_id where id=`
       );
 
     await expect(uri[0]).to.equal(result);
