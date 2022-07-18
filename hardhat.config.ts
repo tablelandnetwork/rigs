bb13f8897b8697427fe5d2dd27f4cedaf2c1ce49
Sander Pick <sanderpick@gmail.com>
2022-07-18 09:36:49 +0200

    flip to waitlist
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -122,7 +122,7 @@ const config: HardhatUserConfig = {
       feeRecipient: "0xEC19654b1c1DfAc2427F06D0fcA991f43C6b1281",
       maxSupply: 3000,
       etherPrice: "0.05",
-      mintPhase: "allowlist",
+      mintPhase: "waitlist",
       tables: {
         tablelandPrivateKey: process.env.ETHEREUM_GOERLI_PRIVATE_KEY,
         tablelandProvider: process.env.ETHEREUM_GOERLI_API_KEY,
