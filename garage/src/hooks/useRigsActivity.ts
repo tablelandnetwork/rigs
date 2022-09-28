6fdeb4e9f39bf47fde15e8516ae98c102df15bc0
Daniel Öhrlund <danielohrlund@gmail.com>
2022-09-28 14:25:29 +0200

    add more mock rig activity data

--- garage/src/hooks/useRigsActivity.ts
+++ garage/src/hooks/useRigsActivity.ts
@@ -63,6 +63,55 @@ export const useRigsActivity = () => {
       action: EventAction.PilotedTrainer,
       timestamp: new Date(),
     },
+    {
+      rigId: "87",
+      thumb:
+        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/1/thumb.png",
+      action: EventAction.PilotedTrainer,
+      timestamp: new Date(),
+    },
+    {
+      rigId: "1746",
+      thumb:
+        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/6/thumb.png",
+      action: EventAction.Parked,
+      timestamp: new Date(),
+    },
+    {
+      rigId: "3",
+      thumb:
+        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/16/thumb.png",
+      action: EventAction.PilotedTrainer,
+      timestamp: new Date(),
+    },
+    {
+      rigId: "720",
+      thumb:
+        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/10/thumb.png",
+      action: EventAction.Parked,
+      timestamp: new Date(),
+    },
+    {
+      rigId: "14",
+      thumb:
+        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/8/thumb.png",
+      action: EventAction.Parked,
+      timestamp: new Date(),
+    },
+    {
+      rigId: "2843",
+      thumb:
+        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/3/thumb.png",
+      action: EventAction.PilotedTrainer,
+      timestamp: new Date(),
+    },
+    {
+      rigId: "1204",
+      thumb:
+        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/13/thumb.png",
+      action: EventAction.PilotedTrainer,
+      timestamp: new Date(),
+    },
   ];
 
   return { events };
