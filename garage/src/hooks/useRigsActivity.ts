f925a5791e85f739cad7223bf0ec6be90004cbad
Daniel Öhrlund <danielohrlund@gmail.com>
2022-10-06 12:39:25 +0200

    update useRigsAcitivity hook to query for real data

--- garage/src/hooks/useRigsActivity.ts
+++ garage/src/hooks/useRigsActivity.ts
@@ -1,118 +1,25 @@
-import { Rig } from "../types";
-
-enum EventAction {
-  PilotedTrainer = "Piloted Trainer",
-  Parked = "Parked",
-}
-
-interface Event {
-  rigId: string;
-  thumb: string;
-  action: EventAction;
-  timestamp: Date;
-}
+import { useEffect, useState } from "react";
+import { Event } from "../types";
+import { useTablelandConnection } from "./useTablelandConnection";
+import { selectRigsActivity } from "../utils/queries";
+import { eventFromRow } from "../utils/xforms";
 
 export const useRigsActivity = () => {
-  const events: Event[] = [
-    {
-      rigId: "87",
-      thumb:
-        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/1/thumb.png",
-      action: EventAction.PilotedTrainer,
-      timestamp: new Date(),
-    },
-    {
-      rigId: "1746",
-      thumb:
-        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/6/thumb.png",
-      action: EventAction.Parked,
-      timestamp: new Date(),
-    },
-    {
-      rigId: "3",
-      thumb:
-        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/16/thumb.png",
-      action: EventAction.PilotedTrainer,
-      timestamp: new Date(),
-    },
-    {
-      rigId: "720",
-      thumb:
-        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/10/thumb.png",
-      action: EventAction.Parked,
-      timestamp: new Date(),
-    },
-    {
-      rigId: "14",
-      thumb:
-        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/8/thumb.png",
-      action: EventAction.Parked,
-      timestamp: new Date(),
-    },
-    {
-      rigId: "2843",
-      thumb:
-        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/3/thumb.png",
-      action: EventAction.PilotedTrainer,
-      timestamp: new Date(),
-    },
-    {
-      rigId: "1204",
-      thumb:
-        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/13/thumb.png",
-      action: EventAction.PilotedTrainer,
-      timestamp: new Date(),
-    },
-    {
-      rigId: "87",
-      thumb:
-        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/1/thumb.png",
-      action: EventAction.PilotedTrainer,
-      timestamp: new Date(),
-    },
-    {
-      rigId: "1746",
-      thumb:
-        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/6/thumb.png",
-      action: EventAction.Parked,
-      timestamp: new Date(),
-    },
-    {
-      rigId: "3",
-      thumb:
-        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/16/thumb.png",
-      action: EventAction.PilotedTrainer,
-      timestamp: new Date(),
-    },
-    {
-      rigId: "720",
-      thumb:
-        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/10/thumb.png",
-      action: EventAction.Parked,
-      timestamp: new Date(),
-    },
-    {
-      rigId: "14",
-      thumb:
-        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/8/thumb.png",
-      action: EventAction.Parked,
-      timestamp: new Date(),
-    },
-    {
-      rigId: "2843",
-      thumb:
-        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/3/thumb.png",
-      action: EventAction.PilotedTrainer,
-      timestamp: new Date(),
-    },
-    {
-      rigId: "1204",
-      thumb:
-        "ipfs://bafybeib3bbctx2gpuzicibprsa3h3zbusogxplccnlgbtmargfnh7bcria/13/thumb.png",
-      action: EventAction.PilotedTrainer,
-      timestamp: new Date(),
-    },
-  ];
+  const { connection: tableland } = useTablelandConnection();
+
+  const [events, setEvents] = useState<Event[]>();
+
+  useEffect(() => {
+    let isCancelled = false;
+
+    tableland.read(selectRigsActivity([], 40)).then((result) => {
+      if (!isCancelled) setEvents(result.rows.map(eventFromRow));
+    });
+
+    return () => {
+      isCancelled = true;
+    };
+  }, [setEvents]);
 
   return { events };
 };
