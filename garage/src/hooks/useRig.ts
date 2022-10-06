f8a4753bd3c08259e90a542478edb6a3b926a40f
Daniel Öhrlund <danielohrlund@gmail.com>
2022-10-06 20:54:51 +0200

    add fake garageStatus data to useRig

--- garage/src/hooks/useRig.ts
+++ garage/src/hooks/useRig.ts
@@ -4,17 +4,47 @@ import { useTablelandConnection } from "./useTablelandConnection";
 import { selectRigs } from "../utils/queries";
 import { rigFromRow } from "../utils/xforms";
 
+interface ParkedGarageStatus {
+  state: "PARKED";
+}
+
+interface ERC721Pilot {
+  contract: string;
+  tokenId: string;
+}
+
+interface FlyingGarageStatus {
+  state: "FLYING";
+  pilot: ERC721Pilot | "training";
+}
+
+type GarageStatus = ParkedGarageStatus | FlyingGarageStatus;
+
+export interface RigWithGarageStatus extends Rig {
+  garageStatus: GarageStatus;
+}
+
+const withFakeGarageStatus = (rig: Rig): RigWithGarageStatus => {
+  return {
+    ...rig,
+    garageStatus: {
+      state: "FLYING",
+      pilot: { contract: "0x....", tokenId: "1" },
+    },
+  };
+};
+
 export const useRig = (id: string) => {
   const { connection: tableland } = useTablelandConnection();
 
-  const [rig, setRig] = useState<Rig>();
+  const [rig, setRig] = useState<RigWithGarageStatus>();
 
   useEffect(() => {
     let isCancelled = false;
 
     tableland.read(selectRigs([id])).then((result) => {
       const rigs = result.rows.map(rigFromRow);
-      if (!isCancelled && rigs.length) setRig(rigs[0]);
+      if (!isCancelled && rigs.length) setRig(withFakeGarageStatus(rigs[0]));
     });
 
     return () => {
