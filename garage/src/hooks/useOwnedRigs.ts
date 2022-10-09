2ddf0c572249906aab321a02f80b5278afddddb8
Daniel Öhrlund <danielohrlund@gmail.com>
2022-10-09 16:51:52 +0200

    update useOwnedRigs hook to use address & interface defined in settings

--- garage/src/hooks/useOwnedRigs.ts
+++ garage/src/hooks/useOwnedRigs.ts
@@ -3,20 +3,16 @@ import { Rig } from "../types";
 import { useAccount, useContractRead } from "wagmi";
 import { useTablelandConnection } from "./useTablelandConnection";
 import { selectRigs } from "../utils/queries";
-import { rigFromRow } from "../utils/xforms"
-
-const RIGS_CONTRACT_ADDRESS = "0x8EAa9AE1Ac89B1c8C8a8104D08C045f78Aadb42D";
-const contractInterface = [
-  "function tokensOfOwner(address) external view returns (uint256[])",
-];
+import { rigFromRow } from "../utils/xforms";
+import { CONTRACT_ADDRESS, CONTRACT_INTERFACE } from "../settings";
 
 export const useOwnedRigs = () => {
   const { address } = useAccount();
   const { connection: tableland } = useTablelandConnection();
 
   const { data } = useContractRead({
-    addressOrName: RIGS_CONTRACT_ADDRESS,
-    contractInterface,
+    addressOrName: CONTRACT_ADDRESS,
+    contractInterface: CONTRACT_INTERFACE,
     functionName: "tokensOfOwner",
     args: address,
   });
