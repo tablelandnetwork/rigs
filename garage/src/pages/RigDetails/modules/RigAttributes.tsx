8efa82203f839b4a42e8582d005efb36d91636d3
Daniel Öhrlund <danielohrlund@gmail.com>
2022-10-13 14:36:26 +0200

    remove RigAttributes table custom code for removing border on last row

--- garage/src/pages/RigDetails/modules/RigAttributes.tsx
+++ garage/src/pages/RigDetails/modules/RigAttributes.tsx
@@ -16,19 +16,11 @@ export const RigAttributes = ({ rig, p, ...props }: RigAttributesProps) => {
         <Tbody>
           {rig.attributes
             .filter(({ traitType }) => traitType !== "VIN")
-            .map((attribute, index) => {
-              const tdProps =
-                index === rig.attributes!.length - 1
-                  ? { borderBottom: "none" }
-                  : index === 0
-                  ? { borderTop: "var(--chakra-borders-1px)" }
-                  : {};
+            .map((attribute) => {
               return (
                 <Tr key={`rig-${rig.id}-attribute-${attribute.traitType}`}>
-                  <Td pl={p} {...tdProps}>
-                    {attribute.traitType}
-                  </Td>
-                  <Td pr={p} {...tdProps} textAlign="right">
+                  <Td pl={p}>{attribute.traitType}</Td>
+                  <Td pr={p} isNumeric>
                     {attribute.value}
                   </Td>
                 </Tr>
