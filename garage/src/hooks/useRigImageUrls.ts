7243a1e05cb39825b2db23bf2ad9d2a537c26a9d
Daniel Öhrlund <danielohrlund@gmail.com>
2022-10-15 18:02:25 +0200

    add temporary image name translation in useRigImageUrls
    to handle the inconsistency between what is in the lookups table and
    file names on ipfs

--- garage/src/hooks/useRigImageUrls.ts
+++ garage/src/hooks/useRigImageUrls.ts
@@ -12,20 +12,21 @@ interface RigImageUrls {
   thumbAlpha?: string;
 }
 
+// TODO(daniel): remove string .replace calls once the data in the looksup table is consistent with ipfs
 export const useRigImageUrls = (rig: Partial<Rig>): RigImageUrls => {
   let result: RigImageUrls = {};
 
   if (rig.image) {
-    result.image = ipfsUriToGatewayUrl(rig.image);
+    result.image = ipfsUriToGatewayUrl(rig.image.replace("_full", ""));
   }
   if (rig.imageAlpha) {
-    result.imageAlpha = ipfsUriToGatewayUrl(rig.imageAlpha);
+    result.imageAlpha = ipfsUriToGatewayUrl(rig.imageAlpha.replace("_full", ""));
   }
   if (rig.thumb) {
-    result.thumb = ipfsUriToGatewayUrl(rig.thumb);
+    result.thumb = ipfsUriToGatewayUrl(rig.thumb.replace("image_", ""));
   }
   if (rig.thumbAlpha) {
-    result.thumbAlpha = ipfsUriToGatewayUrl(rig.thumbAlpha);
+    result.thumbAlpha = ipfsUriToGatewayUrl(rig.thumbAlpha.replace("image_", ""));
   }
 
   return result;
