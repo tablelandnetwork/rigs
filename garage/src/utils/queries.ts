c0eb7661583dca4cb74986e7d431e8d39715a355
Daniel Öhrlund <danielohrlund@gmail.com>
2022-10-15 18:01:53 +0200

    update queries to read table names from @tableland/rigs & use new schema

--- garage/src/utils/queries.ts
+++ garage/src/utils/queries.ts
@@ -1,48 +1,63 @@
-const RIGS = "rigs_5_28";
-const RIGS_ATTRIBUTES = "rig_attributes_5_27";
-const RIG_PILOT_SESSIONS = "rig_pilot_sessions_5_787";
+import { deployments } from "@tableland/rigs/deployments";
+
+const deployment =
+  process.env.NODE_ENV === "development"
+    ? deployments["polygon-mumbai"]
+    : deployments.ethereum;
+
+// TODO(daniel): use this once the data in @tableland/rigs is up to date
+// const { attributesTable, lookupsTable, pilotSessionsTable } = deployment;
+
+const attributesTable = "rig_attributes_5_811";
+const lookupsTable = "lookups_5_812";
+// const pilotSessionsTable = "pilot_sessions_5_813"; // table on goerli
+const pilotSessionsTable = "pilot_sessions_80001_3502"; // table on mumbai used by current contract
+
+const IMAGE_IPFS_URI_SELECT = `'ipfs://'||renders_cid||'/'||rig_id||'/'||image_full_name`;
+const IMAGE_ALPHA_IPFS_URI_SELECT = `'ipfs://'||renders_cid||'/'||rig_id||'/'||image_full_alpha_name`;
+const THUMB_IPFS_URI_SELECT = `'ipfs://'||renders_cid||'/'||rig_id||'/'||image_thumb_name`;
+const THUMB_ALPHA_IPFS_URI_SELECT = `'ipfs://'||renders_cid||'/'||rig_id||'/'||image_thumb_alpha_name`;
 
 export const selectRigs = (ids: string[]): string => {
   return `
   SELECT
-    rigs.id,
-    image,
-    image_alpha,
-    thumb,
-    thumb_alpha,
+    cast(rig_id as text),
+    ${IMAGE_IPFS_URI_SELECT} as "image",
+    ${IMAGE_ALPHA_IPFS_URI_SELECT} as "imageAlpha",
+    ${THUMB_IPFS_URI_SELECT} as "thumb",
+    ${THUMB_ALPHA_IPFS_URI_SELECT} as "thumbAlpha",
     json_group_array(json_object(
       'displayType', display_type,
       'traitType', trait_type,
       'value', value
     )) AS attributes,
-    (SELECT json_object(
-      'contract', pilot_contract,
-      'tokenId', cast(pilot_id as text)
-    ) FROM ${RIG_PILOT_SESSIONS} WHERE rig_id = rigs.id AND end_time IS NULL) AS pilot
-  FROM ${RIGS} AS rigs
-  JOIN ${RIGS_ATTRIBUTES} AS attributes ON rigs.id = attributes.rig_id
-  WHERE rigs.id IN ('${ids.join("', '")}')
-  GROUP BY rigs.id`;
+    (
+      SELECT json_object(
+        'contract', session.pilot_contract,
+        'tokenId', cast(session.pilot_id as text)
+      )
+      FROM ${pilotSessionsTable} AS session
+      WHERE session.rig_id = rig_id AND session.end_time IS NULL
+    ) AS pilot
+  FROM ${attributesTable} AS attributes
+  JOIN ${lookupsTable}
+  WHERE rig_id IN ('${ids.join("', '")}')
+  GROUP BY rig_id`;
 };
 
 export const selectRigWithPilots = (id: string): string => {
   return `
   SELECT
-    rigs.id,
-    image,
-    image_alpha,
-    thumb,
-    thumb_alpha,
-    (
-      SELECT
-        json_group_array(json_object(
-          'displayType', display_type,
-          'traitType', trait_type,
-          'value', value
-        ))
-      FROM ${RIGS_ATTRIBUTES}
-      WHERE rig_id = ${id}
-    ) AS attributes,
+    '${id}' as "rig_id",
+    ${IMAGE_IPFS_URI_SELECT} as "image",
+    ${IMAGE_ALPHA_IPFS_URI_SELECT} as "imageAlpha",
+    ${THUMB_IPFS_URI_SELECT} as "thumb",
+    ${THUMB_ALPHA_IPFS_URI_SELECT} as "thumbAlpha",
+    json_group_array(json_object(
+      'displayType', display_type,
+      'traitType', trait_type,
+      'value', value
+    )) as attributes,
     (
       SELECT
         json_group_array(json_object(
@@ -52,11 +67,12 @@ export const selectRigWithPilots = (id: string): string => {
           'startTime', start_time,
           'endTime', end_time
         ))
-      FROM ${RIG_PILOT_SESSIONS}
+      FROM ${pilotSessionsTable}
       WHERE rig_id = ${id}
     ) AS piloting_sessions
-  FROM ${RIGS} AS rigs
-  WHERE rigs.id = ${id}`;
+  FROM ${attributesTable}
+  JOIN ${lookupsTable}
+  WHERE rig_id = ${id}`;
 };
 
 // TODO(daniel):
@@ -80,16 +96,15 @@ export const selectRigsActivity = (
 
   return `
   SELECT
-    rig_id,
-    thumb,
-    image,
+    cast(rig_id as text),
+    ${THUMB_IPFS_URI_SELECT} as "thumb",
     pilot_contract,
     pilot_id,
     start_time,
     end_time,
     max(start_time, coalesce(end_time, 0)) as "timestamp"
-  FROM ${RIG_PILOT_SESSIONS} AS sessions
-  JOIN ${RIGS} AS rigs ON sessions.rig_id = rigs.id
+  FROM ${pilotSessionsTable} AS sessions
+  JOIN ${lookupsTable}
   ${whereClause}
   ORDER BY timestamp DESC
   LIMIT ${first}
@@ -102,30 +117,30 @@ export const selectStats = (blockNumber: number): string => {
   return `
   SELECT
   (
-    SELECT count(*) FROM
-    ${RIGS}
+    SELECT count(distinct(rig_id)) FROM
+    ${attributesTable}
   ) AS num_rigs,
   (
     SELECT count(*) FROM (
       SELECT DISTINCT(rig_id)
-      FROM ${RIG_PILOT_SESSIONS}
+      FROM ${pilotSessionsTable}
       WHERE end_time IS NULL
     )
   ) AS num_rigs_in_flight,
   (
     SELECT count(*) FROM (
       SELECT DISTINCT pilot_contract, pilot_id
-      FROM ${RIG_PILOT_SESSIONS}
+      FROM ${pilotSessionsTable}
     )
   ) AS num_pilots,
   (
-    SELECT sum(coalesce(end_time, ${blockNumber}) - start_time)
-    FROM ${RIG_PILOT_SESSIONS}
+    SELECT coalesce(sum(coalesce(end_time, ${blockNumber}) - start_time), 0)
+    FROM ${pilotSessionsTable}
   ) AS total_flight_time,
   (
-    SELECT avg(coalesce(end_time, ${blockNumber}) - start_time)
-    FROM ${RIG_PILOT_SESSIONS}
+    SELECT coalesce(avg(coalesce(end_time, ${blockNumber}) - start_time), 0)
+    FROM ${pilotSessionsTable}
   ) AS avg_flight_time
-  FROM ${RIGS}
+  FROM ${attributesTable}
   LIMIT 1;`;
 };
