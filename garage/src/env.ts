84e30f3c63f40d0daa052b8b63e81c6a992c8348
Daniel Öhrlund <danielohrlund@gmail.com>
2022-10-16 17:26:21 +0200

    clean up env deployment definition

--- garage/src/env.ts
+++ garage/src/env.ts
@@ -12,12 +12,15 @@ export const blockExplorerBaseUrl = isDevelopment
   ? "https://mumbai.polygonscan.com"
   : "https://etherscan.io";
 
-// TODO(daniel): once the data in @tableland/rigs is up to date just reference
-// deployments["polygon-mumbai"] and deployments.ethereum
-export const deployment: Pick<
+type SimplifiedDeployment = Pick<
   RigsDeployment,
   "attributesTable" | "lookupsTable" | "pilotSessionsTable" | "contractAddress"
-> = isDevelopment
+>;
+
+// TODO(daniel): once the data in @tableland/rigs is up to date just reference
+// deployments["polygon-mumbai"] and deployments.ethereum
+// and remove SimplifiedDeployment
+export const deployment: SimplifiedDeployment = isDevelopment
   ? {
       attributesTable: "rig_attributes_80001_3507",
       lookupsTable: "lookups_80001_3508",
