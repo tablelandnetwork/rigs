a1b3d6d77827ff93bcaa1da707ccd82bca871e5f
Sander Pick <sanderpick@gmail.com>
2022-10-15 21:18:39 -0700

    deploy new test contract to mumbai
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- ethereum/deployments.ts
+++ ethereum/deployments.ts
@@ -19,7 +19,6 @@ export interface RigsDeployment {
   layersTable: string;
   attributesTable: string;
   lookupsTable: string;
-  pilotSessionsTable: string;
   displayAttributes: boolean;
 }
 
@@ -40,22 +39,20 @@ export const deployments: RigsDeployments = {
     layersTable: "layers_5_29",
     attributesTable: "rig_attributes_5_27",
     lookupsTable: "", // TODO: Fill me.
-    pilotSessionsTable: "", // TODO: Fill me.
     displayAttributes: true,
   },
   // testnets
   "polygon-mumbai": {
-    contractAddress: "0x875fc1205Cb81A1F38fC4deEC44390c2aC5f5890",
-    royaltyContractAddress: "0x13287B498F61c735221e11eD2ED6808597d996f3",
-    tablelandChain: "ethereum-goerli",
+    contractAddress: "0x2f9EE58e25D1AcA55841D98f7f1b0aEbD11750Bc",
+    royaltyContractAddress: "0x7829b4aB8200F50dC1B146Ddba9Ef4501E3cFd47",
+    tablelandChain: "polygon-mumbai",
     tablelandHost: "https://testnet.tableland.network",
-    contractTable: "rigs_contract_5_57",
-    allowlistTable: "rigs_allowlist_5_59",
-    partsTable: "parts_5_21",
-    layersTable: "layers_5_19",
-    attributesTable: "rig_attributes_5_20",
-    lookupsTable: "", // TODO: Fill me.
-    pilotSessionsTable: "pilot_sessions_80001_3498",
+    contractTable: "rigs_contract_80001_3513",
+    allowlistTable: "rigs_allowlist_80001_3514",
+    partsTable: "",
+    layersTable: "",
+    attributesTable: "rig_attributes_80001_3507",
+    lookupsTable: "lookups_80001_3508",
     displayAttributes: true,
   },
   localhost: {
@@ -69,7 +66,6 @@ export const deployments: RigsDeployments = {
     layersTable: "",
     attributesTable: "",
     lookupsTable: "",
-    pilotSessionsTable: "",
     displayAttributes: true,
   },
 };
