632c47791683d45c8c6c7891705008762aaf6bcf
Sander Pick <sanderpick@gmail.com>
2022-10-16 16:49:01 -0700

    migrate to slugs, use vercel
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- animation_url/svelte.config.js
+++ animation_url/svelte.config.js
@@ -1,17 +1,19 @@
-import adapter from 'sveltejs-adapter-ipfs';
+// import adapter from 'sveltejs-adapter-ipfs';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
 
-  kit: {
-    adapter: adapter({
-      fallback: "index.html"
-    })
-  }
+	kit: {
+		adapter: adapter()
+		// adapter: adapter({
+		// 	fallback: 'index.html'
+		// })
+	}
 };
 
 export default config;
