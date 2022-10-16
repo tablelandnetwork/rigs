d3c804ef53258be78a36cd6f040aa440696c9cb9
Daniel Öhrlund <danielohrlund@gmail.com>
2022-10-16 14:02:21 +0200

    remove no longer needed hack for matching ipfs filenames with lookup tbl

--- garage/src/hooks/useRigImageUrls.ts
+++ garage/src/hooks/useRigImageUrls.ts
@@ -13,21 +13,20 @@ interface RigImageUrls {
   thumbAlpha?: string;
 }
 
-// TODO(daniel): remove string .replace calls once the data in the looksup table is consistent with ipfs
 export const useRigImageUrls = (rig: Partial<Rig>): RigImageUrls => {
   let result: RigImageUrls = {};
 
   if (rig.image) {
-    result.image = ipfsUriToGatewayUrl(rig.image.replace("_full", ""));
+    result.image = ipfsUriToGatewayUrl(rig.image);
   }
   if (rig.imageAlpha) {
-    result.imageAlpha = ipfsUriToGatewayUrl(rig.imageAlpha.replace("_full", ""));
+    result.imageAlpha = ipfsUriToGatewayUrl(rig.imageAlpha);
   }
   if (rig.thumb) {
-    result.thumb = ipfsUriToGatewayUrl(rig.thumb.replace("image_", ""));
+    result.thumb = ipfsUriToGatewayUrl(rig.thumb);
   }
   if (rig.thumbAlpha) {
-    result.thumbAlpha = ipfsUriToGatewayUrl(rig.thumbAlpha.replace("image_", ""));
+    result.thumbAlpha = ipfsUriToGatewayUrl(rig.thumbAlpha);
   }
 
   return result;
