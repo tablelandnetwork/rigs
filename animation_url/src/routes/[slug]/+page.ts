2822cf1a93fa21cc44e1a07ae4273aac1238814f
Sander Pick <sanderpick@gmail.com>
2022-10-17 14:58:10 -0700

    format
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- animation_url/src/routes/[slug]/+page.ts
+++ animation_url/src/routes/[slug]/+page.ts
@@ -1,44 +1,43 @@
-// import { ethers, BigNumber } from 'ethers';
-import { connect } from '@tableland/sdk';
+import { connect } from "@tableland/sdk";
 
-const attributesTable = 'rig_attributes_80001_3507';
-const lookupsTable = 'lookups_80001_3508';
-const pilotSessionsTable = 'pilot_sessions_80001_3515';
-const ipfsGatewayUri = 'https://nftstorage.link/ipfs/';
+const attributesTable = "rig_attributes_80001_3507";
+const lookupsTable = "lookups_80001_3508";
+const pilotSessionsTable = "pilot_sessions_80001_3515";
+const ipfsGatewayUri = "https://nftstorage.link/ipfs/";
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ url }) {
-	const tableland = connect({ chain: 'polygon-mumbai' });
+  const tableland = connect({ chain: "polygon-mumbai" });
 
-	// get rig id, allowing for .html suffix
-	let rigId = url.pathname;
-	const parts = rigId.replace(/^\/|\/$/g, '').split('.');
-	rigId = parts[0];
+  // get rig id, allowing for .html suffix
+  let rigId = url.pathname;
+  const parts = rigId.replace(/^\/|\/$/g, "").split(".");
+  rigId = parts[0];
 
-	// get image
-	const stm = `select json_object(
+  // get image
+  const stm = `select json_object(
     'image','ipfs://'||renders_cid||'/'||rig_id||'/'||image_medium_name
   ) from ${attributesTable} join ${lookupsTable} where rig_id=${rigId} group by rig_id;`;
-	const metadata: any = await tableland.read(stm, {
-		output: 'objects',
-		extract: true,
-		unwrap: true
-	});
-	const httpUri = ipfsGatewayUri + metadata.image.slice(7);
+  const metadata: any = await tableland.read(stm, {
+    output: "objects",
+    extract: true,
+    unwrap: true,
+  });
+  const httpUri = ipfsGatewayUri + metadata.image.slice(7);
 
-	// get pilot
-	let pilot;
-	const sessions: any = await tableland.read(
-		`SELECT end_time FROM ${pilotSessionsTable} WHERE rig_id = ${rigId} AND end_time is null;`,
-		{ output: 'objects' }
-	);
-	if (sessions && sessions.length > 0) {
-		pilot = 'trainer_pilot.svg';
-	}
+  // get pilot
+  let pilot;
+  const sessions: any = await tableland.read(
+    `SELECT end_time FROM ${pilotSessionsTable} WHERE rig_id = ${rigId} AND end_time is null;`,
+    { output: "objects" }
+  );
+  if (sessions && sessions.length > 0) {
+    pilot = "trainer_pilot.svg";
+  }
 
-	return {
-		imageUrl: httpUri,
-		badges: [],
-		pilot
-	};
+  return {
+    imageUrl: httpUri,
+    badges: [],
+    pilot,
+  };
 }
