fa695ce10c07b845687e4bd07d4b4c22fdd4710e
Sander Pick <sanderpick@gmail.com>
2022-10-17 11:30:07 -0700

    use end_time for piloted query
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- animation_url/svelte.config.js
+++ animation_url/svelte.config.js
@@ -1,5 +1,4 @@
-// import adapter from 'sveltejs-adapter-ipfs';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,9 +9,6 @@ const config = {
 
 	kit: {
 		adapter: adapter()
-		// adapter: adapter({
-		// 	fallback: 'index.html'
-		// })
 	}
 };
 
