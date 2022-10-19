6d89946e7c7a339ad88a0b632c1a0c84e34838bb
Daniel Öhrlund <danielohrlund@gmail.com>
2022-10-19 23:01:39 +0200

    simplify/improve responsive behavior for the Dashboard page

--- garage/src/pages/Dashboard/index.tsx
+++ garage/src/pages/Dashboard/index.tsx
@@ -18,32 +18,23 @@ export const Dashboard = () => {
     <Flex
       direction="column"
       align="center"
-      justify="stretch"
       width="100%"
       minHeight={`calc(100vh - ${TOPBAR_HEIGHT})`}
     >
       <Flex
         direction={{ base: "column", lg: "row" }}
-        pt={GRID_GAP}
+        p={GRID_GAP}
         gap={GRID_GAP}
-        px={GRID_GAP}
         align={{ base: "stretch", lg: "start" }}
         maxWidth="1385px"
         width="100%"
-        height="100%"
+        minHeight={`calc(100vh - ${TOPBAR_HEIGHT})`}
       >
-        <Flex
-          direction="column"
-          gap={GRID_GAP}
-          flexGrow="2"
-          align="stretch"
-          width="100%"
-          height="100%"
-        >
+        <Flex direction="column" gap={GRID_GAP} align="stretch" width="100%">
           <RigsInventory {...MODULE_PROPS} />
           <Stats {...MODULE_PROPS} />
         </Flex>
-        <Box flexGrow="1" flexShrink="0">
+        <Box>
           <Activity {...MODULE_PROPS} />
         </Box>
       </Flex>
