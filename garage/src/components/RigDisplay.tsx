d6a4feea9dd373d4bdb53ecf7d1ed3a8012a1a5d
Daniel Öhrlund <danielohrlund@gmail.com>
2022-10-27 16:32:20 +0200

    update RigDisplay to support showing a loading indicator at pilot spot

--- garage/src/components/RigDisplay.tsx
+++ garage/src/components/RigDisplay.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Flex, Image, Spinner } from "@chakra-ui/react";
 import { TrainerPilot } from "./TrainerPilot";
 import { useRigImageUrls } from "../hooks/useRigImageUrls";
 import { Rig } from "../types";
@@ -10,18 +10,34 @@ type BorderWidth = React.ComponentProps<typeof Box>["borderWidth"];
 type Props = React.ComponentProps<typeof Box> & {
   rig: Rig;
   pilotNFT?: NFT;
+  loading?: boolean;
   pilotBorderWidth?: BorderWidth;
+};
+
+const LoadingPilot = (props: React.ComponentProps<typeof Box>) => {
+  return (
+    <Flex
+      justify="center"
+      align="center"
+      backgroundColor="primary"
+      {...props}
+      sx={{ aspectRatio: "1/1" }}
+    >
+      <Spinner size="lg" color="black" />
+    </Flex>
+  );
 };
 
 export const RigDisplay = ({
   rig,
   pilotNFT,
   pilotBorderWidth,
+  loading = false,
   ...props
 }: Props) => {
   const { thumb } = useRigImageUrls(rig);
 
-  const showPilot = rig.currentPilot;
+  const showPilot = rig.currentPilot || loading;
   const imageUrl = pilotNFT?.imageUrl;
 
   const borderWidth = pilotBorderWidth ?? "1px";
@@ -39,7 +55,9 @@ export const RigDisplay = ({
       <Image src={thumb} />
       <Box position="absolute" right="0" bottom="0" width="25%">
         {showPilot ? (
-          imageUrl ? (
+          loading ? (
+            <LoadingPilot {...pilotProps} />
+          ) : imageUrl ? (
             <Image src={pilotNFT.imageUrl} {...pilotProps} bgColor="paper" />
           ) : (
             <TrainerPilot {...pilotProps} />
