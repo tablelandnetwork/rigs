d57a46b2739976cfe2b15bf037ab409078287039
Daniel Öhrlund <danielohrlund@gmail.com>
2022-10-27 15:58:11 +0200

    update RequiresWalletConnection wrapper to check tableland for token

--- garage/src/components/RequiresWalletConnection.tsx
+++ garage/src/components/RequiresWalletConnection.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAccount } from "wagmi";
+import { useTablelandConnection } from "../hooks/useTablelandConnection";
 
 interface RequiresWalletConnectionProps {
   children: React.ReactNode;
@@ -10,10 +11,11 @@ export const RequiresWalletConnection = ({
   children,
 }: RequiresWalletConnectionProps) => {
   const { isConnected } = useAccount();
+  const { connection: tableland } = useTablelandConnection();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!isConnected) navigate("/");
+    if (!isConnected || !tableland.token) navigate("/");
   }, [isConnected, navigate]);
 
   return <>{isConnected ? children : null}</>;
