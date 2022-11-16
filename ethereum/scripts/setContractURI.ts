b30cbf2cad9626c3c0c61f967ba184892f998965
Sander Pick <sanderpick@gmail.com>
2022-11-16 01:10:19 -0800

    add set contract uri script
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- ethereum/scripts/setContractURI.ts
+++ ethereum/scripts/setContractURI.ts
@@ -1,9 +1,9 @@
 import { ethers, network, rigsDeployment } from "hardhat";
 import { TablelandRigs } from "../typechain-types";
-import { getURITemplate } from "../helpers/uris";
+import { getContractURI } from "../helpers/uris";
 
 async function main() {
-  console.log(`\nUpdating base URI on '${network.name}'...`);
+  console.log(`\nUpdating contract URI on '${network.name}'...`);
 
   // Get proxy owner account
   const [account] = await ethers.getSigners();
@@ -18,31 +18,22 @@ async function main() {
   console.log(`Using address '${rigsDeployment.contractAddress}'`);
 
   // Ensure we can build URI template
-  if (rigsDeployment.attributesTable === "") {
-    throw Error(`missing attributes table entry in deployments`);
-  }
-  if (rigsDeployment.lookupsTable === "") {
-    throw Error(`missing lookups table entry in deployments`);
-  }
-  if (rigsDeployment.pilotSessionsTable === "") {
-    throw Error(`missing pilot sessions table entry in deployments`);
+  if (rigsDeployment.contractTable === "") {
+    throw Error(`missing contract table entry in deployments`);
   }
 
   // Update URI template
   const rigs = (await ethers.getContractFactory("TablelandRigs")).attach(
     rigsDeployment.contractAddress
   ) as TablelandRigs;
-  const uriTemplate = getURITemplate(
+  const contractURI = getContractURI(
     rigsDeployment.tablelandHost,
-    rigsDeployment.attributesTable,
-    rigsDeployment.lookupsTable,
-    rigsDeployment.pilotSessionsTable,
-    rigsDeployment.displayAttributes
+    rigsDeployment.contractTable
   );
-  const tx = await rigs.setURITemplate(uriTemplate);
+  const tx = await rigs.setContractURI(contractURI);
   const receipt = await tx.wait();
   console.log(
-    `URI template set to '${uriTemplate}' with txn '${receipt.transactionHash}'`
+    `contract URI set to '${contractURI}' with txn '${receipt.transactionHash}'`
   );
 }
 
