515e679736d4882c7572019daf95e5048726a130
Dan Buchholz <dbuchholz30@gmail.com>
2022-12-06 15:19:11 -0800

    feat: remove session FTR on force park

--- ethereum/contracts/TablelandRigPilots.sol
+++ ethereum/contracts/TablelandRigPilots.sol
@@ -11,7 +11,6 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@tableland/evm/contracts/utils/SQLHelpers.sol";
 import "@tableland/evm/contracts/utils/TablelandDeployments.sol";
 import "./ITablelandRigPilots.sol";
-import "./TablelandRigs.sol";
 
 /**
  * @dev Implementation of {ITablelandRigPilots}.
@@ -319,7 +318,7 @@ contract TablelandRigPilots is
             _pilotIndex[pilotData] != 0 &&
             _pilotIndex[pilotData] != tokenId &&
             _pilotStatus(_pilotIndex[pilotData]) != GarageStatus.PARKED
-        ) parkRig(sender, _pilotIndex[pilotData]);
+        ) parkRig(_pilotIndex[pilotData], false);
 
         // If the Rig is training, end its training session (no parking required) to then open a new pilot session
         // Pilot has completed training at this point (training validation is checked above)
@@ -387,7 +386,7 @@ contract TablelandRigPilots is
     /**
      * @dev See {ITablelandRigPilots-parkRig}.
      */
-    function parkRig(address sender, uint256 tokenId) public onlyParent {
+    function parkRig(uint256 tokenId, bool force) public onlyParent {
         // Ensure Rig is currently in-flight
         GarageStatus status = _pilotStatus(tokenId);
         if (
@@ -397,17 +396,12 @@ contract TablelandRigPilots is
         // Use `setters` for setting SQL update values, and `startTime` is used for checking training completion status
         string memory setters;
         uint64 startTime = pilotStartTime(tokenId);
-        // Also, if the `sender` is *not* the token owner nor the parent contract, the caller is the parent contract's _owner_
-        // This *only* happens if the caller is force parking a Rig, thus, flight time rewards are revoked
-        // Check this by calling the parent contract and retrieving the `ownerOf(tokenId)`
-        TablelandRigs rigs = TablelandRigs(parent());
-        bool isForcePark = sender != rigs.ownerOf(tokenId);
-        bool isUntrained = status == GarageStatus.TRAINING &&
+        bool trainingIncomplete = status == GarageStatus.TRAINING &&
             !(block.number >= startTime + _PILOT_TRAINING_DURATION);
         // Pilot training is incomplete; reset the training pilot such that the Rig must train again
-        if (isUntrained) _setPilotData(uint16(tokenId), 0, 0);
+        if (trainingIncomplete) _setPilotData(uint16(tokenId), 0, 0);
         // If the pilot is untrained or being force parked, there are zero flight time rewards
-        if (isUntrained || isForcePark) {
+        if (trainingIncomplete || force) {
             // Update the row in pilot sessions table with its `end_time` equal to the `start_time`
             setters = string.concat(
                 "end_time=",
