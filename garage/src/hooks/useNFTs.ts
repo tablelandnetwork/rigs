c850f16ed2be26d257f92ae0f3ac9771353edcae
Daniel Öhrlund <danielohrlund@gmail.com>
2022-12-12 16:51:11 +0100

    rename NFT.imageSvgData -> imageData

--- garage/src/hooks/useNFTs.ts
+++ garage/src/hooks/useNFTs.ts
@@ -40,7 +40,7 @@ export interface NFT {
   tokenId: string;
   name?: string;
   imageUrl?: string;
-  imageSvgData?: string;
+  imageData?: string;
 }
 
 const toNFT = (data: Nft): NFT => {
@@ -52,7 +52,7 @@ const toNFT = (data: Nft): NFT => {
     tokenId,
     name: title,
     imageUrl: media.length ? media[0].gateway : undefined,
-    imageSvgData: data.rawMetadata?.svg_image_data,
+    imageData: data.rawMetadata?.svg_image_data,
   };
 };
 
