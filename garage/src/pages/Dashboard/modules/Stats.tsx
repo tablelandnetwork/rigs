6b408151aec952c40eb1d5b089b031c8ce0931fd
Daniel Öhrlund <danielohrlund@gmail.com>
2022-12-19 13:02:28 +0100

    add Pilots stats tab to Dashboard

--- garage/src/pages/Dashboard/modules/Stats.tsx
+++ garage/src/pages/Dashboard/modules/Stats.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useMemo } from "react";
 import {
   Box,
   Heading,
   Flex,
   GridItem,
+  VStack,
+  Image,
   SimpleGrid,
   Spinner,
   Stack,
   Tab,
+  Table,
+  Thead,
+  Tbody,
+  Th,
+  Tr,
+  Td,
   Tabs,
   TabList,
   TabPanel,
@@ -15,7 +23,14 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { useAccount, useBlockNumber } from "wagmi";
-import { useAccountStats, useStats, Stat } from "../../../hooks/useRigStats";
+import {
+  useAccountStats,
+  useStats,
+  useTopActivePilotCollections,
+  useTopFtPilotCollections,
+  Stat,
+} from "../../../hooks/useRigStats";
+import { useNFTCollections, Collection } from "../../../hooks/useNFTs";
 import { prettyNumber } from "../../../utils/fmt";
 
 const StatItem = ({ name, value }: { name: string; value: number }) => {
@@ -60,11 +75,56 @@ const StatGrid = ({ stats }: { stats?: Stat[] }) => {
   );
 };
 
+const CollectionToplist = ({
+  stat,
+  data,
+}: {
+  stat: string;
+  data: { collection: Collection; stat: string | number }[];
+}) => {
+  return (
+    <Table>
+      <Thead>
+        <Tr>
+          <Th colSpan={2}>Collection</Th>
+          <Th isNumeric>{stat}</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {data?.slice(0, 10).map(({ collection, stat }, index) => {
+          return (
+            <Tr key={`pilot-${index}`}>
+              <Td width="30px" px={0}>
+                <Image src={collection?.imageUrl} width="30px" height="30px" />
+              </Td>
+              <Td>{collection?.name}</Td>
+              <Td isNumeric>{stat}</Td>
+            </Tr>
+          );
+        })}
+      </Tbody>
+    </Table>
+  );
+};
+
 export const Stats = (props: React.ComponentProps<typeof Box>) => {
   const { address } = useAccount();
   const { data: currentBlockNumber } = useBlockNumber();
   const { stats } = useStats(currentBlockNumber);
   const { stats: accountStats } = useAccountStats(currentBlockNumber, address);
+  const { stats: pilotStats } = useTopActivePilotCollections();
+  const { stats: ftStats } = useTopFtPilotCollections(currentBlockNumber);
+
+  const contracts = useMemo(
+    () => pilotStats?.slice(0, 10).map((v) => v.contractAddress),
+    [pilotStats]
+  );
+  const { collections } = useNFTCollections(contracts);
+  const collectionLookup = useMemo(() => {
+    return Object.fromEntries(
+      collections?.map((v) => [v.contractAddress.toLowerCase(), v]) || []
+    );
+  }, [collections]);
 
   return (
     <Flex direction="column" sx={{ height: "100%" }} {...props}>
@@ -73,6 +133,7 @@ export const Stats = (props: React.ComponentProps<typeof Box>) => {
         <TabList>
           <Tab>Global</Tab>
           <Tab>You</Tab>
+          <Tab>Pilots</Tab>
         </TabList>
 
         <TabPanels>
@@ -81,6 +142,43 @@ export const Stats = (props: React.ComponentProps<typeof Box>) => {
           </TabPanel>
           <TabPanel px={0}>
             <StatGrid stats={accountStats} />
+          </TabPanel>
+          <TabPanel fontSize="0.8em" px={0}>
+            <Flex
+              width="100%"
+              gap={8}
+              pt={2}
+              direction={{ base: "column", md: "row" }}
+            >
+              <VStack flexGrow="1" align="start">
+                <Heading>Top collections</Heading>
+                {pilotStats && (
+                  <CollectionToplist
+                    stat="Num. pilots"
+                    data={pilotStats
+                      .slice(0, 10)
+                      .map(({ contractAddress, count }) => {
+                        const collection = collectionLookup[contractAddress];
+                        return { collection, stat: count };
+                      })}
+                  />
+                )}
+              </VStack>
+              <VStack flexGrow="1" align="start">
+                <Heading>Most FT earned</Heading>
+                {ftStats && (
+                  <CollectionToplist
+                    stat="FT"
+                    data={ftStats
+                      .slice(0, 10)
+                      .map(({ contractAddress, ft }) => {
+                        const collection = collectionLookup[contractAddress];
+                        return { collection, stat: prettyNumber(ft) };
+                      })}
+                  />
+                )}
+              </VStack>
+            </Flex>
           </TabPanel>
         </TabPanels>
       </Tabs>
