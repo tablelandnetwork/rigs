d254b938ac99ada428fadb89bcb14af0446d1f11
Daniel Öhrlund <danielohrlund@gmail.com>
2023-01-04 17:51:14 +0100

    add rig search input/quick nav to topbar

--- garage/src/Topbar.tsx
+++ garage/src/Topbar.tsx
@@ -1,17 +1,78 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Button, Flex, Image, Show, Spacer, Text } from "@chakra-ui/react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  Button,
+  Flex,
+  HStack,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Kbd,
+  Show,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 import { TablelandConnectButton } from "./components/TablelandConnectButton";
 import logo from "./assets/tableland.svg";
 import { useCurrentRoute } from "./hooks/useCurrentRoute";
+import { useKeysDown } from "./hooks/useKeysDown";
 
 export const TOPBAR_HEIGHT = "80px";
 
+const useFocus = () => {
+  const [focused, setFocused] = useState(false);
+  const onFocus = useCallback(() => setFocused(true), [setFocused]);
+  const onBlur = useCallback(() => setFocused(false), [setFocused]);
+
+  return { focused, onFocus, onBlur };
+};
+
 export const Topbar = () => {
   const route = useCurrentRoute();
+  const navigate = useNavigate();
+  const keysDown = useKeysDown();
 
   const isEnter = route?.route.key === "ENTER";
   const bgColor = isEnter ? "primaryLight" : "primary";
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (
+      (keysDown.has("Meta") || keysDown.has("Control")) &&
+      keysDown.has("k")
+    ) {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    }
+  }, [keysDown]);
+
+  const [searchValue, setSearchValue] = useState("");
+  const onInputChanged = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearchValue(e.target.value);
+    },
+    [setSearchValue]
+  );
+
+  const onInputKeypressed = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === "Enter") {
+        const rigId = parseInt(searchValue, 10);
+        if (rigId > 0 && rigId < 3001) {
+          navigate(`/rigs/${rigId}`);
+          setSearchValue("");
+          inputRef.current?.blur();
+          window.scrollTo(0, 0);
+        }
+      }
+    },
+    [navigate, searchValue, setSearchValue]
+  );
+
+  const { focused, onFocus, onBlur } = useFocus();
 
   return (
     <Flex
@@ -26,21 +87,81 @@ export const Topbar = () => {
       px={8}
       py={4}
     >
-      <Image
-        src={logo}
-        sx={{ maxWidth: { base: "50px", md: "100%" } }}
-        mr={2}
-      />
-      <Show above="sm">
+      <Link to="/dashboard">
+        <Image
+          src={logo}
+          sx={{ maxWidth: { base: "50px", md: "100%" } }}
+          mr={2}
+        />
+      </Link>
+      <Show above="md">
         <Text variant="orbitron" fontSize="20">
           Garage
         </Text>
       </Show>
       <Flex justify="space-between" align="center" width="100%" ml={8}>
         {!isEnter ? (
-          <Button variant="solid" as={Link} to="/dashboard">
-            Dashboard
-          </Button>
+          <Flex
+            align="center"
+            gap={4}
+            mr={4}
+            flexGrow="1"
+            justify="space-between"
+          >
+            <Show above="md">
+              <Button
+                variant="solid"
+                as={Link}
+                to="/dashboard"
+                flexGrow="0"
+                flexShrink="0"
+              >
+                Dashboard
+              </Button>
+            </Show>
+            <InputGroup maxWidth="300px" minWidth="120px" flexGrow="1">
+              <InputLeftElement
+                pointerEvents="none"
+                children={<SearchIcon color="#FFFFFFAA" />}
+              />
+              <Input
+                type="number"
+                placeholder="Rig #"
+                color="#FFFFFFAA"
+                bgColor="#00000033"
+                borderColor="#00000033"
+                focusBorderColor="#00000022"
+                ref={inputRef}
+                value={searchValue}
+                onChange={onInputChanged}
+                onKeyPress={onInputKeypressed}
+                onFocus={onFocus}
+                onBlur={onBlur}
+                min="0"
+                max="3000"
+                pr={{ base: "12px", sm: "48px" }}
+                size="md"
+              />
+              <Show above="sm">
+                <InputRightElement
+                  pointerEvents="none"
+                  width="48px"
+                  children={
+                    <HStack color="#FFFFFFAA" mr={3}>
+                      {focused ? (
+                        <Kbd>Enter</Kbd>
+                      ) : (
+                        <>
+                          <Kbd>⌘</Kbd>
+                          <Kbd>K</Kbd>
+                        </>
+                      )}
+                    </HStack>
+                  }
+                />
+              </Show>
+            </InputGroup>
+          </Flex>
         ) : (
           <Spacer />
         )}
