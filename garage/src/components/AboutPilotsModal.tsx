c414c366200b3a5c3e326661c51970f8a6a0f584
Daniel Öhrlund <danielohrlund@gmail.com>
2023-01-09 21:49:01 +0100

    proposed new copy for about pilots modal

--- garage/src/components/AboutPilotsModal.tsx
+++ garage/src/components/AboutPilotsModal.tsx
@@ -27,35 +27,43 @@ export const AboutPilotsModal = ({ isOpen, onClose }: ModalProps) => {
     <Modal isOpen={isOpen} onClose={onClose} size="xl">
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>About Pilots</ModalHeader>
+        <ModalHeader>About piloting and FT</ModalHeader>
         <ModalCloseButton />
-        <ModalBody p={6}>
-          <VStack>
+        <ModalBody px={6} pb={6}>
+          <VStack align="start">
             <Image src={garage} />
-            <Heading as="h4" py={6}>
-              Training Rigs
+            <Heading as="h4" pt={6}>
+              Flight Time (FT)
             </Heading>
-            <Text pb={4}>
-              Before your Rig can handle a real pilot (an ERC721 token that you
-              own), it needs to accumulate enough FT with the trainer.
-              <Text as="em" color="primaryLight">
-                {" "}
-                This will take about 30 days.
-              </Text>
+            <Text pb={3}>
+              Flight Time is a non-transferrable reputation metric for
+              the Tableland ecosystem (that is stored and tracked in Tableland!)
             </Text>
-            <UnorderedList px={8}>
-              <ListItem pb={2}>
-                <Text as="em" color="primaryLight">
-                  In-flight Rigs ARE NOT sellable or transferable!
-                </Text>{" "}
-                Your Rig may be auto-parked if it's listed on a marketplace.
-              </ListItem>
-              <ListItem>
-                If your Rig has a real pilot that you sold or transferred, your
-                Rig will be auto-parked if the new owner uses it as a pilot of a
-                different Rig.
-              </ListItem>
-            </UnorderedList>
+            <Text>
+              As a Rig owner you can earn FT by taking your Rigs flying.
+              A Rig that is in-flight cannot be transferred or sold,
+              and as a reward for taking your Rig off the market you earn FT (also referred to as soft-staking, the NFT doesn't leave your wallet but it is locked).
+            </Text>
+            <Heading as="h4" pt={6}>
+              Pilots and training
+            </Heading>
+            <Text pb={3}>
+              Every Rig must go through a training phase with a trainer pilot to re-learn how to fly.
+              Training takes <Text as="em" color="primaryLight">172,800 blocks </Text>
+              (about 30 days).
+            </Text>
+            <Text>
+              After training you can use any ERC721 token that you
+              own to pilot your Rigs. Pilot history is stored on Tableland,
+              and can be queried and reflected in metadata
+            </Text>
+            <Heading as="h4" pt={6}>
+              How do I train/pilot my Rigs?
+            </Heading>
+            <Text>
+              Training, piloting and parking all require an on-chain transaction.
+              This means that you will have to sign a transaction and pay a small gas every time you fee for every action you take.
+            </Text>
           </VStack>
         </ModalBody>
         <ModalFooter>
