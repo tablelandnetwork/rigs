5e04909045f7a5867898f0e733d5a28d2fd56174
Daniel Öhrlund <danielohrlund@gmail.com>
2023-01-26 15:13:42 +0100

    update Validator initialization to use database config

--- garage/src/hooks/useTablelandConnection.ts
+++ garage/src/hooks/useTablelandConnection.ts
@@ -2,9 +2,7 @@ import { Database, Validator } from "@tableland/sdk";
 import { chain } from "../env";
 
 const db = Database.readOnly(chain.id);
-// TODO this doesn't work:
-// new Validator(db.config)
-const validator = new Validator({ baseUrl: "https://tableland.network" });
+const validator = new Validator(db.config);
 const data = { db, validator };
 
 export const useTablelandConnection = () => {
