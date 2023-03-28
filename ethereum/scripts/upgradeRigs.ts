024d569e8e8df1b63a6296d7ed65cef5e91beaee
Sander Pick <sanderpick@gmail.com>
2023-03-28 16:06:42 -0700

    manually initialize new impls after upgrade
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- ethereum/scripts/upgradeRigs.ts
+++ ethereum/scripts/upgradeRigs.ts
@@ -1,6 +1,7 @@
-import { ethers, upgrades, network, rigsDeployment } from "hardhat";
+import { ethers, upgrades, network, rigsDeployment, rigsConfig } from "hardhat";
 import type { TablelandRigs } from "../typechain-types";
 import assert from "assert";
+import { BigNumber, utils } from "ethers";
 
 async function main() {
   console.log(`\nUpgrading on '${network.name}'...`);
@@ -38,6 +39,19 @@ async function main() {
   // Check new implementation
   const impl2 = await upgrades.erc1967.getImplementationAddress(rigs.address);
   console.log("New implementation address:", impl2);
+
+  // TMP: Manually initialize impl. Once RIG-30 is done we can remove this.
+  const rigsImpl = Factory.attach(impl2) as TablelandRigs;
+  const tx = await rigsImpl.initialize(
+    BigNumber.from(rigsConfig.maxSupply),
+    utils.parseEther(rigsConfig.etherPrice),
+    rigsConfig.feeRecipient,
+    rigsDeployment.royaltyContractAddress,
+    "0xcb3e8f37cd26c729d6ed94f151517549508020b191327dc78c7657bbd2872a50",
+    "0xe45a43693f3688d2d97d6e933af863b80f24517da45579e157c6da40b71d77a2"
+  );
+  const receipt = await tx.wait();
+  console.log(`Initialized new impl with txn '${receipt.transactionHash}'`);
 
   // Warn if implementation did not change, ie, nothing happened.
   if (impl === impl2) {
