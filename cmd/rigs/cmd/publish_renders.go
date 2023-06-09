05b1c7a49152c30604ed41e3cb79099ab945f79b
Aaron Sutula <asutula@users.noreply.github.com>
2023-06-09 11:08:09 -0600

    naming fix
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>

--- cmd/rigs/cmd/publish_renders.go
+++ cmd/rigs/cmd/publish_renders.go
@@ -9,14 +9,14 @@ import (
 )
 
 func init() {
-	rendersNewCmd.Flags().String("renders-path", "./renders", "path to the rendered images")
-	rendersNewCmd.Flags().Int("concurrency", 5, "number of concurrent uploads to car storage service")
-	rendersNewCmd.Flags().Duration("rate-limit", time.Millisecond*1000, "rate limit for uploads to car storage service")
+	rendersCmd.Flags().String("renders-path", "./renders", "path to the rendered images")
+	rendersCmd.Flags().Int("concurrency", 5, "number of concurrent uploads to car storage service")
+	rendersCmd.Flags().Duration("rate-limit", time.Millisecond*1000, "rate limit for uploads to car storage service")
 
-	publishCmd.AddCommand(rendersNewCmd)
+	publishCmd.AddCommand(rendersCmd)
 }
 
-var rendersNewCmd = &cobra.Command{
+var rendersCmd = &cobra.Command{
 	Use:   "renders",
 	Short: "Publish rig renders to a car storage service",
 	Run: func(cmd *cobra.Command, args []string) {
