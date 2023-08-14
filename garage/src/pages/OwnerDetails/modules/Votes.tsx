b451955c95411f09cc5ac00e9ac867dfaa6c3199
Daniel Öhrlund <danielohrlund@gmail.com>
2023-08-14 22:06:38 +0200

    improve the OwnerDetails votes table on mobile screens
    by moving the choices column to a separate row instead, and adjusting
    the column widths

--- garage/src/pages/OwnerDetails/modules/Votes.tsx
+++ garage/src/pages/OwnerDetails/modules/Votes.tsx
@@ -12,6 +12,8 @@ import {
   Tr,
   Td,
   VStack,
+  Show,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import { prettyNumber } from "../../../utils/fmt";
 import { Vote } from "../../../hooks/useOwnerVotes";
@@ -24,17 +26,28 @@ interface VotesProps extends React.ComponentProps<typeof Box> {
 const truncateChoiceString = (s: string, l: number = 80) =>
   s.slice(0, l) + (s.length > l ? "..." : "");
 
+const noBorderBottom = { borderBottom: "none" };
+
 export const Votes = ({ votes, p, ...props }: VotesProps) => {
+  const isMobile = useBreakpointValue({
+    base: true,
+    sm: false,
+  });
+
+  const mainRowColAttrs = isMobile ? noBorderBottom : {};
+
   return (
     <VStack align="stretch" spacing={4} pt={p} {...props}>
       <Heading px={p}>Votes</Heading>
-      <Table>
+      <Table variant="simple">
         <Thead>
           <Tr>
-            <Th pl={p} width="190px">
+            <Th pl={p} maxWidth={{ base: "100%", sm: "230px" }}>
               Proposal
             </Th>
-            <Th>Choices</Th>
+            <Show above="sm">
+              <Th>Choices</Th>
+            </Show>
             <Th pr={p} isNumeric>
               Voting Power
             </Th>
@@ -55,17 +68,30 @@ export const Votes = ({ votes, p, ...props }: VotesProps) => {
                 )
                 .join(", ");
               return (
-                <Tr key={`vote-${index}`}>
-                  <Td pl={p} width="130px">
-                    <Link to={`/proposals/${proposal.id}`}>
-                      {proposal.name}
-                    </Link>
-                  </Td>
-                  <Td title={choiceString}>{truncateChoiceString(choiceString)}</Td>
-                  <Td pr={p} isNumeric>
-                    {prettyNumber(ft)}
-                  </Td>
-                </Tr>
+                <React.Fragment key={`vote-${index}`}>
+                  <Tr>
+                    <Td pl={p} maxWidth="230px" {...mainRowColAttrs}>
+                      <Link to={`/proposals/${proposal.id}`}>
+                        {proposal.name}
+                      </Link>
+                    </Td>
+                    <Show above="sm">
+                      <Td title={choiceString}>
+                        {truncateChoiceString(choiceString)}
+                      </Td>
+                    </Show>
+                    <Td pr={p} isNumeric {...mainRowColAttrs}>
+                      {prettyNumber(ft)}
+                    </Td>
+                  </Tr>
+                  <Show below="sm">
+                    <Tr>
+                      <Td px={p} pt={0} colSpan={2} title={choiceString}>
+                        {truncateChoiceString(choiceString)}
+                      </Td>
+                    </Tr>
+                  </Show>
+                </React.Fragment>
               );
             })}
         </Tbody>
