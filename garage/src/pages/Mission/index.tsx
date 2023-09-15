93a21d0fe357b82d25903d3902f37844467e8e37
Daniel Öhrlund <danielohrlund@gmail.com>
2023-09-15 20:57:57 +0200

    add link to owner profile from mission details page contributions list

--- garage/src/pages/Mission/index.tsx
+++ garage/src/pages/Mission/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import {
   Box,
   Button,
@@ -136,7 +137,11 @@ const Contributions = ({
                     : truncateWalletAddress(contribution.contributor);
                 return (
                   <Tr key={`contribution-${idx}`}>
-                    <Td pl={p}>{contributor}</Td>
+                    <Td pl={p}>
+                      <Link to={`/owner/${contribution.contributor}`}>
+                        {contributor}
+                      </Link>
+                    </Td>
                     <Td pr={p}>
                       {prettySubmissionStatus(contribution.status)}
                     </Td>
